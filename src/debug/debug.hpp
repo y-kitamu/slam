a7e16c2faff90e78d4161ac1ff2400e305f8d9ef
Kitamura <ymyk6602@gmail.com>
2022-07-31 12:22:41 +0900

    create dev environment

---
+++ src/debug/debug.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file debug.hpp
+ * @brief
+ * @author Yusuke Kitamura <ymyk6602@gmail.com>
+ * @date 2022-07-31 11:14:26
+ */
+#ifndef DEBUG_HPP__
+#define DEBUG_HPP__
+
+
+
+#endif // DEBUG_HPP__
---
+++ src/slam.hpp
@@ -0,0 +1,13 @@
+/**
+ * @file slam.hpp
+ * @brief
+ * @author Yusuke Kitamura <ymyk6602@gmail.com>
+ * @date 2022-07-31 09:29:10
+ */
+#ifndef SLAM_HPP__
+#define SLAM_HPP__
+
+
+#include "debug/dbug.hpp"
+
+#endif // SLAM_HPP__
