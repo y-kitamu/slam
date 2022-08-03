0b2084594f3466aab697dfd021a1a4dca9a79e43
Kitamura <ymyk6602@gmail.com>
2022-08-03 10:08:38 +0900

    wip

--- src/debug/debug.hpp
+++ src/debug/debug.hpp
@@ -7,4 +7,35 @@
 #ifndef DEBUG_HPP__
 #define DEBUG_HPP__
 
-#endif // DEBUG_HPP__
+#include <filesystem>
+
+#include <fmt/chrono.h>
+#include <fmt/format.h>
+
+#include "custom_format.hpp"
+
+namespace {
+
+namespace fs = std::filesystem;
+
+}  // namespace
+
+const fs::path PROJECT_ROOT = fs::path(__FILE__).parent_path().parent_path();
+
+#define _log(loglevel, fmtstr, ...)                                                             \
+    std::cout << fmt::format("[{:%Y-%m-%d %H:%M:%S} {} File: {}, Line: {}] ",                   \
+                             fmt::localtime(std::time(nullptr)), loglevel,                      \
+                             fs::relative(fs::path(__FILE__), PROJECT_ROOT).string(), __LINE__) \
+              << fmt::format(fmtstr __VA_OPT__(, ) __VA_ARGS__) << std::endl;
+
+#ifdef NDEBUG
+#define logd(fmtstr, ...)
+#else
+#define logd(fmtstr, ...) _log("DEBUG", fmtstr __VA_OPT__(, ) __VA_ARGS__)
+#endif  // NDEBUG
+
+#define logw(fmtstr, ...) _log("WARNING", fmtstr __VA_OPT__(, ) __VA_ARGS__)
+#define loge(fmtstr, ...) _log("ERROR", fmtstr __VA_OPT__(, ) __VA_ARGS__)
+
+
+#endif  // DEBUG_HPP__
